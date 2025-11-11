40aaaddbe4d7b8ab3db871d8ccb5114b2936cbf9
nnourgalal <ngalal2801@gmail.com>
2025-11-11 09:31:41 +0200

    Update Student.h

--- Student.h
+++ Student.h
@@ -4,7 +4,18 @@
 #include "Person.h"
 #include <string>
 #using namespace std;
-//#write student class here
+
+class Student {
+private:
+    int yearLevel;
+    string major;
+public:
+    Student(int yl, string m) {
+        yearLevel = yl;
+        major = m;
+        void display();
+    }
+};
 
 
 
