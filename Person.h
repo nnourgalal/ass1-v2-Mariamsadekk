aa06fe86cd8d3d2bde80d658196af558679d9c31
nnourgalal <ngalal2801@gmail.com>
2025-11-11 09:45:07 +0200

    Update Person.h

--- Person.h
+++ Person.h
@@ -4,8 +4,23 @@
 #include <string>
 using namespace std;
 
-//#write Person class here
+class Person {
+private:
+    string name;
+    int id;
+public:
+    Person(string n, int id) {
+        name = n;
+        id = id;
+        void display();
 
+    }
+
+    getPerson() {
+        return Person
+    }
+
+};
 
 
 
